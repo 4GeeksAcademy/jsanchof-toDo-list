62f14b65f74045f9ed8015bde9c3129d63f88ec2
Jose Sancho <jose.sancho89@gmail.com>
2025-03-07 02:29:29 +0000

    -v0.2

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,64 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+function TodoItem({ label, deleteTodo }) {
+	return (
+		<div className="item-list">
+			<span className="todo-text">{label}</span>
+			<i className="fas fa-trash-alt"  onClick={deleteTodo}/>
+		</div>
+	)
+}
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	const [todos, setTodos] = useState([]);
+	const [todoInput, setTodoInput] = useState("");
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	useEffect(() => {
+		localStorage.setItem("todos", JSON.stringify(todos));
+	}, [todos]);
+	return (
+		<div className="container">
+			<form
+				onSubmit={(ev) => {
+					ev.preventDefault();
+					if (todoInput.length > 0) {
+						setTodos([{
+							label: todoInput,
+							isDone: false
+						},
+						...todos,
+						]);
+						setTodoInput("");
+					}
+				}}
+
+				className="container d-flex flex-column align-items-center">
+
+				<h1>To Do List</h1>
+				<input
+					className="form-control form-control-lg"
+					type="text"
+					placeholder="Whats pending for today?"
+					aria-describedby="input field"
+					value={todoInput}
+					onChange={(ev) => setTodoInput(ev.target.value)}
+				></input>
+				{todos.map((item, idx) =>
+					<TodoItem
+						key={idx}
+						label={item.label}
+						toggleToDo={() =>
+							setTodos(
+								todos.toSpliced(idx, 1, {
+									label: item.label,
+								})
+							)}
+						deleteTodo={() => setTodos(todos.toSpliced(idx, 1))}
+					/>
+				)}
+			</form>
+			<small className="taskCounter">Task pending: {todos.length}</small>
 		</div>
 	);
 };
